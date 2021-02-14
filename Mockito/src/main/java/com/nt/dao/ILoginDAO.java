9d046843e483b9037124a2b029b535fb36864218
TL <TL@gmail.com>
2021-02-14 07:49:18 +0530

    deleted

--- Mockito/src/main/java/com/nt/dao/ILoginDAO.java
+++ Mockito/src/main/java/com/nt/dao/ILoginDAO.java
@@ -4,6 +4,5 @@ public interface ILoginDAO {
 
 	public int Authenticate(String username,String pwd);
 	public int Auth(int id, String name);
-	public String Maha(int ide);
-	public Double Transfer(int src, int dest);
+	
 }
